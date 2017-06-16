a98fa71ba6faf3274619754c89a4a65679fc98d8
Ryan Canfield <ryancanfield@me.com>
2017-06-16 13:34:20 -0700

    Initial commit

---
+++ android/app/src/main/java/com/ryancanfield/test/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.ryancanfield.test.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp87eabcb6e49b4352be32583329bca8ae://d4-kvv.canfie1d.test.exp.direct:80";
+
+}
