dc0a6797adc466f7c7b74c0210fedb2b953cb617
Ryan Canfield <ryancanfield@me.com>
2017-06-28 14:27:18 -0700

    Make the whole app ¯\_(ツ)_/¯

--- App.js
+++ App.js
@@ -1,16 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TextInput
+} from 'react-native';
+import Marquee from '@remobile/react-native-marquee';
+
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      height: Dimensions.get('window').height,
+      width: Dimensions.get('window').width,
+      textString: ''
+    }
+  }
+
+  clearInput() {
+    this.setState({textString: ''});
+  }
+
+  onLayout(event) {
+    const newHeight = event.nativeEvent.layout.height;
+    const newWidth = event.nativeEvent.layout.width;
+
+    this.setState({
+      height: newHeight,
+      width: newWidth
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>WE'RE IN BUSINESS 😎</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    const height = this.state.height;
+    const width = this.state.width;
+    const textString = this.state.textString;
+
+    if (height > width) {
+      return (
+        <View style={styles.container} onLayout={(event) => this.onLayout(event)}>
+          <Text style={styles.instruction}>Input text to display on your marquee:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(textString) => this.setState({textString})}
+            value={this.state.textString}
+          />
+          <Text style={styles.instruction}>Rotate your phone to display the message.</Text>
+          <Button
+            onPress={this.clearInput.bind(this)}
+            title="CLEAR"
+            color="#ff0000"
+            accessibilityLabel="Clear the input"
+          />
+        </View>
+      );
+    } else {
+      let renderMessage = () => {
+        if (textString !== '') {
+          return (
+            <Marquee style={styles.marquee}>
+              {textString}
+            </Marquee>
+          );
+        }
+
+        return (
+           <Text style={styles.instruction}>Type a message in portrait mode :)</Text>
+        );
+      }
+      return (
+        <View style={textString !== '' ? styles.marqueeContainer : styles.container} onLayout={(event) => this.onLayout(event)}>
+          {renderMessage()}
+        </View>
+      );
+    }
   }
 }
 
@@ -21,4 +89,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  marqueeContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    marginLeft: '10%',
+    paddingRight: 15,
+    paddingLeft: 15,
+    height: 60,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'rgba(0,0,0,.8)',
+    backgroundColor: 'rgba(0,0,0,.1)',
+    fontSize: 20,
+    color: '#ff0000',
+  },
+  instruction: {
+    fontSize: 16,
+    color: '#555',
+    lineHeight: 28,
+  },
+  marquee: {
+    fontSize: 200,
+    color: '#555'
+  }
 });
--- android/app/src/main/java/com/ryancanfield/test/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/ryancanfield/test/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.ryancanfield.test.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp87eabcb6e49b4352be32583329bca8ae://d4-kvv.canfie1d.test.exp.direct:80";
+  public static final String DEVELOPMENT_URL = "exp87eabcb6e49b4352be32583329bca8ae://qy-hqa.canfie1d.marquee.exp.direct:80";
 
 }
